c8757bb1bf1334a289afda47cd450ffcb3052cad
michael woodroof <michaelwoodroof@outlook.com>
2020-10-05 10:04:31 +0100

    Updates

---
+++ precache-manifest.78824d09bd0e1aa8fb6a58e391d72643.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ba7503a7970929901b5cec8d54aa42",
+    "url": "/Simple-React-Website/index.html"
+  },
+  {
+    "revision": "09adc1a5be6a3a889bc8",
+    "url": "/Simple-React-Website/static/css/main.b8bed5f5.chunk.css"
+  },
+  {
+    "revision": "61b68ce0aa46e934b8d6",
+    "url": "/Simple-React-Website/static/js/2.b7ed6587.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-React-Website/static/js/2.b7ed6587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09adc1a5be6a3a889bc8",
+    "url": "/Simple-React-Website/static/js/main.c1146a6c.chunk.js"
+  },
+  {
+    "revision": "3deea40383864d4a5243",
+    "url": "/Simple-React-Website/static/js/runtime-main.89f5abe8.js"
+  },
+  {
+    "revision": "f3b7d629c80a85788fe1cc7377576fec",
+    "url": "/Simple-React-Website/static/media/call.f3b7d629.svg"
+  },
+  {
+    "revision": "ba66d505714a3f97ce9d552f65708d95",
+    "url": "/Simple-React-Website/static/media/email.ba66d505.svg"
+  },
+  {
+    "revision": "9aaf36cdaf3c6e7c3461cb58b304d0ac",
+    "url": "/Simple-React-Website/static/media/linkedin.9aaf36cd.svg"
+  },
+  {
+    "revision": "4b7ac033b57a18affff391e60e233a78",
+    "url": "/Simple-React-Website/static/media/melwood.4b7ac033.svg"
+  },
+  {
+    "revision": "9aaaba9f2563c94818b1db817ee16a9d",
+    "url": "/Simple-React-Website/static/media/placeholder.9aaaba9f.svg"
+  },
+  {
+    "revision": "13a5442aeaa829de5da91ee1a5d937e7",
+    "url": "/Simple-React-Website/static/media/trophy.13a5442a.svg"
+  },
+  {
+    "revision": "5dde72a2d9cb50e04dc10338e254cff8",
+    "url": "/Simple-React-Website/static/media/twitter.5dde72a2.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Simple-React-Website/precache-manifest.78824d09bd0e1aa8fb6a58e391d72643.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Simple-React-Website/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
