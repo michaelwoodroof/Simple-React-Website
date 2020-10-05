a77aa5e698c73178639cc57c5a2214112168287d
michael woodroof <michaelwoodroof@outlook.com>
2020-10-05 09:47:20 +0100

    Final Touches

--- src/Achievements.js
+++ src/Achievements.js
@@ -9,7 +9,8 @@ function Achievements() {
             <h1>Achievements</h1>
             <div className="Achievements">
                 {data["achievements"].map( (obj) =>
-                    <Achievement key={obj.name} desc={obj.desc} />
+                    <Achievement key={obj.name} name={obj.name} desc={obj.desc}
+                    month={obj.month} year={obj.year} />
                 )}
             </div>
         </div>
@@ -24,7 +25,9 @@ class Achievement extends React.Component {
                 <div class="CircleBacker">
                     <img className="Trophy" src={trophy} alt="trophy"/>
                 </div>
-                <p className="AchievementDesc">{this.props.desc}</p>
+                <h1 style={{fontSize: "12pt"}}>{this.props.name}</h1>
+                <p style={{textAlign: "left"}}>{this.props.desc}</p>
+                <p className="AchievementMonthYear">{this.props.month} {this.props.year}</p>
             </div>
         )
     }
--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import './css/Contact.css'
+import call from './assets/images/call.svg'
+import email from './assets/images/email.svg'
 
 function Contact() {
     return (
         <div className="Content">
             <h1>Contact Us</h1>
-            <div>
-            </div>
+            <IndividualContact icon="call" info="01234 567890"/>
+            <IndividualContact icon="email" info="madeupemail@madeup.com"/>
         </div>
+    )
+}
+
+class IndividualContact extends React.Component {
+    render() {
+        var img;
+        if (this.props.icon === "call") {
+            img = <Phone />
+        } else {
+            img = <Email />
+        }
+        return (
+            <div className="ContactContainer">
+                {img}
+                <p>{this.props.info}</p>
+            </div>
+        )
+    }
+}
+
+function Phone() {
+    return (
+        <img src={call} />
+    )
+}
+
+function Email() {
+    return (
+        <img src={email} />
     )
 }
 
